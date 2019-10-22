1eb3818577febe84d82254fca9a5c620f3f55317
Larry Shaffer <lshaffer@federal.planet.com>
2019-10-22 12:15:28 -0600

    Update setup.py: bump to v0.3.0, add Planet (c), constrain Py version

--- setup.py
+++ setup.py
@@ -9,23 +9,27 @@ with codecs.open('README', encoding='utf-8') as f:
 
 setup(
     name='mgrspy',
-    version='0.2.2',
+    version='0.3.0',
     install_requires=['GDAL>=1.10.0', 'future'],
-    author='Alexander Bruy',
-    author_email='abruy@boundlessgeo.com',
+    author='Planet Federal',
+    author_email='info@federal.planet.com',
     description='Convert WGS84 coordinates to MGRS and back',
-    long_description=(readme_text),
+    long_description=readme_text,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v2 or later (GPLv2+)',
         'Operating System :: OS Independent',
-        'Topic :: Scientific/Engineering :: GIS'
+        'Topic :: Scientific/Engineering :: GIS',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
     ],
     license="GPLv2+",
     keywords='mgrs wgs gis coordinate conversion',
-    url='https://github.com/boundlessgeo/mgrspy',
+    url='https://github.com/planetfederal/mgrspy',
     package_dir={'': '.'},
     test_suite='tests.suite',
-    packages=['mgrspy',]
+    packages=['mgrspy'],
+    python_requires='>=2.7, >=3.4, <4',
 )
